5dc7a5ad3f299de1ddbf4ecf5ac51b2a65db15d3
Sompalli Jayadeep <sompallijaydeep8@gmail.com>
2026-07-19 18:40:14 +0530

    Add interactive Start Menu with outside click handling

--- js/core/taskbar.js
+++ js/core/taskbar.js
@@ -1,4 +1,6 @@
 const clock = document.getElementById("taskbar-clock");
+const startButton = document.getElementById("start-btn");
+const startMenu = document.getElementById("start-menu");
 
 function updateClock() {
     const now = new Date();
@@ -14,3 +16,11 @@ function updateClock() {
 updateClock();
 
 setInterval(updateClock, 60000);
+startButton.addEventListener("click", function () {
+    event.stopPropagation();
+    startMenu.classList.toggle("hidden");
+
+});
+document.addEventListener("click", function () {
+    startMenu.classList.add("hidden");
+});
